59bfc224b11457f8abe98bd6c9ae7a32ce63b3c0
all-hack <unhack.all.hack@gmail.com>
2015-07-05 21:36:15 -0400

    ex13: Parameters, Unpacking, Variables: unpacking ARGV

---
+++ ex13.rb
@@ -0,0 +1,7 @@
+# ex13: Parameters, Unpacking, Variables 
+
+first, second, third = ARGV
+
+puts "Your first varaibale is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third varaible is: #{third}"
