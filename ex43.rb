2c73bd38ffd746223ac42c46147d8e1e2a1f7069
all-hack <unhack.all.hack@gmail.com>
2015-07-07 21:39:31 -0400

    ex43: basic object oriented analysis and design: FULL GAME, practice with classes

---
+++ ex43.rb
@@ -0,0 +1,22 @@
+# ex43: Basic object oriented analysis and design 
+
+# learnrubythehardway.org/book/ex43.html
+
+# ZED PROCESS Top down 
+## Write or draw about the problem 
+### list all of the nouns and verbs in your writing and drawings
+### how are they related 
+## Extract key concepts from 1 and research them 
+## Create a class hierarchy and object map for the concepts 
+## Code the classes and a test to run them 
+## repeat and refine 
+
+# ZED PROCESS Bottom Up
+## take a small piece of the problem; hack on some code and get it to run barely
+## refine the code into something more formal with classes and automated tests. 
+## extract the key concepts you're using and try to find research for them
+## write a description of whats really going on 
+## go back and refine the code, possibly throwing it out and starting over 
+## repeat, moving on to some other piece of the problem 
+
+
